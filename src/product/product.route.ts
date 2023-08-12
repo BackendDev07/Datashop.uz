1d907f3d988558f2e081069fa46a8bc2cc430109
BackEndDev07 <browhith3.uz@gmail.com>
2023-08-12 17:21:09 +0500

    Update

--- src/product/product.route.ts
+++ src/product/product.route.ts
@@ -4,10 +4,6 @@ import productController from './product.controller'
 import { userVerify } from '../common/user-verify.middleware'
 import bodyValidate from './../common/body-validate.middleware'
 import productModel from './product.model'
-import ratingModel from '../rating/rating.model'
-import ratingController from '../rating/rating.controller'
-import reviewModel from '../review/review.model'
-import reviewController from '../review/review.controller'
 
 const router = Router()
 
@@ -32,14 +28,5 @@ router.get('/:id', productController.getProductById)
 
 // Rating
 
-router.post('/:id/rating', userVerify(['admin']), bodyValidate(ratingModel.rating), ratingController.createRating)
-router.put('/rating/:id', userVerify(['admin']), bodyValidate(ratingModel.updateRating), ratingController.updateRating)
-router.delete('/rating/:id', userVerify(['admin']), ratingController.deleteRating)
-
-// Review
-
-router.post('/:id/review', userVerify(['admin']), bodyValidate(reviewModel.review), reviewController.createReview)
-router.delete('/review/:id', userVerify(['admin']), reviewController.deleteReview)
-router.put('/review/:id', userVerify(['admin']), bodyValidate(reviewModel.updateReview), reviewController.updateReview)
 
 export default router
